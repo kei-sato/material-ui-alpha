f6641784a9b19f52a0ac698bd49ca6a807544a43
Kei Sato <kei-sato@meeetol.com>
2016-08-18 09:39:32 +0900

    add comments to webpack-dev-server.config.js

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -5,43 +5,38 @@ const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 const config = {
-  // Entry points to the project
   entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/app/app.js'),
+    'webpack/hot/dev-server',                 // HMR (Hot Module Replacement). ブラウザ更新せずに変更が適用される設定
+    'webpack/hot/only-dev-server',            // syntax error時リロードされるのを防ぐ
+    path.join(__dirname, '/src/app/app.js'),  // エントリーポイント
   ],
-  // Server Configuration options
+  // サーバー設定オプション
   devServer: {
-    contentBase: 'src/www', // Relative directory for base of server
-    devtool: 'eval',
-    hot: true, // Live-reload
-    inline: true,
-    port: 3000, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
+    contentBase : 'src/www',   // ドキュメントルート（Apacheでいう/var/www/html）的な。
+    devtool     : 'eval',
+    hot         : true,
+    inline      : true,
+    port        : 3000,
+    host        : 'localhost', // localhost以外からアクセスする必要がある場合は'0.0.0.0'
   },
   devtool: 'eval',
   output: {
-    path: buildPath, // Path of output file
+    path: buildPath,    // buildディレクトリに出力する
     filename: 'app.js',
   },
   plugins: [
-    // Enables Hot Modules Replacement
-    new webpack.HotModuleReplacementPlugin(),
-    // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
-    // Moves files
-    new TransferWebpackPlugin([
+    new webpack.HotModuleReplacementPlugin(), // HMR
+    new webpack.NoErrorsPlugin(),             // エラー時に警告は出すが、コンパイルは止めない
+    new TransferWebpackPlugin([               // wwwにあるファイルをビルドディレクトリに含める
       {from: 'www'},
     ], path.resolve(__dirname, 'src')),
   ],
   module: {
     loaders: [
       {
-        // React-hot loader and
-        test: /\.js$/, // All .js files
-        loaders: ['react-hot', 'babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
-        exclude: [nodeModulesPath],
+        test: /\.js$/,                          // jsファイルをコンパイル
+        loaders: ['react-hot', 'babel-loader'], // react-hotはHMR. babel-loaderはjsxとes6(es7)
+        exclude: [nodeModulesPath],             // node_modules以下のファイルはコンパイル対象から外す
       },
     ],
   },
