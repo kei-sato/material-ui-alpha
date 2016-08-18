f33339477b8721f95e23117c3045b4d7eaecc7c1
Kei Sato <kei-sato@meeetol.com>
2016-08-18 09:40:18 +0900

    minimal webpack-dev-server config

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,50 +1,41 @@
 const webpack = require('webpack');
 const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 const config = {
-  // Entry points to the project
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/src/app/app.js'),
-  ],
-  // Server Configuration options
-  devServer: {
-    contentBase: 'src/www', // Relative directory for base of server
-    devtool: 'eval',
-    hot: true, // Live-reload
-    inline: true,
-    port: 3000, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
-  },
-  devtool: 'eval',
+  // エントリーポイント
+  entry: [path.join(__dirname, '/src/app/app.js')],
+
+  // 出力ファイル
   output: {
-    path: buildPath, // Path of output file
+    path    : path.resolve(__dirname, 'build'),
     filename: 'app.js',
   },
+
+  module: {
+    // コンパイル設定
+    loaders: [
+      {
+        test   : /\.js$/,           // jsファイルをコンパイル
+        loaders: ['babel-loader'],  // babel-loderはjsxとes6(es7)のコンパイラ
+        exclude: /node_modules/,    // node_modules以下のファイルはコンパイル対象から外す
+      },
+    ],
+  },
+
+  // サーバーの設定
+  devServer: {
+    contentBase: 'src/www',
+    port       : 3000,
+  },
+
+  // コンパイル以外の処理
   plugins: [
-    // Enables Hot Modules Replacement
-    new webpack.HotModuleReplacementPlugin(),
-    // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
-    // Moves files
+    // src/wwwにあるファイルをビルドディレクトリに含める
     new TransferWebpackPlugin([
       {from: 'www'},
     ], path.resolve(__dirname, 'src')),
   ],
-  module: {
-    loaders: [
-      {
-        // React-hot loader and
-        test: /\.js$/, // All .js files
-        loaders: ['react-hot', 'babel-loader'], // react-hot is like browser sync and babel loads jsx and es6-7
-        exclude: [nodeModulesPath],
-      },
-    ],
-  },
 };
 
 module.exports = config;
